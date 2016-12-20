94ff9654287576d0d6ca519e40197c84eb214a53
Krudeboy51 <krudeboy51@krudeboy51s-imac.local>
2016-12-20 00:10:42 -0500

    Initial Commit

---
+++ NYC Handbook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NYC Handbook
+//
+//  Created by Krudeboy51 on 12/20/16.
+//  Copyright © 2016 KingEnterprise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
